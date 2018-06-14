d0454abb1b79e5d7d2a10feede0dbc6f16d91645
AndreyZn <andrey.zn.ua@gmail.com>
2018-06-14 14:37:06 +0300

    Modification Products

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -42,11 +42,11 @@ class ProductController extends AppController {
     //Получить галерею
     $gallery = \R::findAll('gallery', 'product_id=?', [$product->id]);
 
-
-    //TODO Получить модификацию товара
+    //Получить модификацию товара
+    $mods = \R::findAll('modification', 'product_id=?', [$product->id]);
 
     $this->setMeta($product->title, $product->description, $product->keywords);
-    $this->set(compact('product', 'related', 'gallery', 'recentlyViewed', 'breadcrumbs'));
+    $this->set(compact('product', 'related', 'gallery', 'recentlyViewed', 'breadcrumbs', 'mods'));
   }
 
 }
