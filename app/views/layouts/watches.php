a7c8f0102958f48bf91a3bfca7cbf3875500b557
AndreyZn <andrey.zn.ua@gmail.com>
2018-06-16 14:45:42 +0300

    Search Ajax

--- app/views/layouts/watches.php
+++ app/views/layouts/watches.php
@@ -78,10 +78,17 @@
                 <div class="clearfix"> </div>
             </div>
             <div class="col-md-3 header-right">
+
+                <!-- typeahead plugin -->
                 <div class="search-bar">
-                    <input type="text" value="Search" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search';}">
-                    <input type="submit" value="">
+                    <form action="search" method="get" autocomplete="off">
+                        <input type="text" class="typeahead" id="typeahead" name="s">
+                        <input type="submit" value="">
+                    </form>
+                    <!--<input type="text" value="Search" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search';}">
+                    <input type="submit" value="">-->
                 </div>
+                <!-- /typeahead plugin -->
             </div>
             <div class="clearfix"> </div>
         </div>
@@ -187,6 +194,7 @@
 
 <script src="js/jquery-1.11.0.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
+<script src="js/typeahead.bundle.js"></script>
 <!--dropdown-->
 <script src="js/jquery.easydropdown.js"></script>
 <!--Slider-Starts-Here-->
