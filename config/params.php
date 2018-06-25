43eb7b3089a54bb645d675434db8bb46ae7481ff
AndreyZn <andrey.zn.ua@gmail.com>
2018-06-25 10:29:03 +0300

    Send Mail

--- config/params.php
+++ config/params.php
@@ -1,9 +1,12 @@
 <?php
 
 return [
-  'admin_email'   => 'admin@mail.com',
-  'shop_name'     => 'Магазин eshop.loc',
-  'pagination'    => 3,
-  'smtp_login'    => '',
-  'smtp_password' => '',
+  'admin_email' => 'andrey.zn.ua@gmail.com',
+  'shop_name' => 'Магазин eshop.loc',
+  'pagination' => 3,
+  'smtp_host' => 'smtp.ukr.net',
+  'smtp_port' => '2525',
+  'smtp_protocol' => 'ssl',
+  'smtp_login' => 'andrey_znayomskiy@ukr.net',
+  'smtp_password' => 'Akella28Konan',
 ];
